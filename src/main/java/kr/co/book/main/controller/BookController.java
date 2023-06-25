1f5200addb7b969721089f4b14e89a4b43c1283e
DESKTOP-NN89PMQ\ParkHeeJoon <vzoo8204@naver.com>
2023-06-26 08:27:57 +0900

    알람 -> 메인페이지 이동

--- src/main/java/kr/co/book/main/controller/BookController.java
+++ src/main/java/kr/co/book/main/controller/BookController.java
@@ -3,10 +3,18 @@ package kr.co.book.main.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
 
+	@RequestMapping(value="/")
+	public String main() {
+		logger.info("main으로 이동");
+		return "index";
+	}
+	
+	
 }
