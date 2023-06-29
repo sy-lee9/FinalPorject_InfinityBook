2fef2ff51267ca76045a8a12b6308e0173761243
choieunyoung <coo810@naver.com>
2023-06-29 16:57:52 +0900

    독서모임 상세보기

--- src/main/java/kr/co/book/club/dao/ClubDAO.java
+++ src/main/java/kr/co/book/club/dao/ClubDAO.java
@@ -1,5 +1,24 @@
 package kr.co.book.club.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.book.club.dto.ClubDTO;
+@Mapper
 public interface ClubDAO {
+
+	String getPhotoName(String email);
+
+	void fileWrite(String ori_photo_name, String new_photo_name, String IDX);
+
+	void clubApply(HashMap<String, String> params);
+
+	ArrayList<ClubDTO> clubList();
+
+	ClubDTO clubDetail(String clubIdx);
+
+	ArrayList<String> clubDetailPhoto(String clubIdx);
 
 }
