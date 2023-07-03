21cb84b5934fd29060165fd057e29a029caa4a35
leemyunga <leemyunga0927@gmail.com>
2023-07-03 17:51:42 +0900

    adminBook 시작

--- src/main/java/kr/co/book/admin/controller/AdminBookController.java
+++ src/main/java/kr/co/book/admin/controller/AdminBookController.java
@@ -1,19 +1,38 @@
 package kr.co.book.admin.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.book.admin.service.AdminBookService;
 
 @Controller
 public class AdminBookController {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
 	
+	@Autowired AdminBookService adminBookService;
+	
 	@RequestMapping("/adminBook")
 	public String adminBook(){
 		return "/admin/book";
+	}
+	
+	@RequestMapping("/adminBookList.ajax")
+	@ResponseBody
+	public HashMap<String, Object> adminBookListAjax(@RequestParam String page,@RequestParam String searchText){
+		logger.info("searchText : "+searchText);
+		HashMap<String, Object> adminBook = adminBookService.adminBookList(page,searchText);
+		
+		return adminBook;
 	}
 	
 }
--- src/main/java/kr/co/book/admin/service/AdminBookService.java
+++ src/main/java/kr/co/book/admin/service/AdminBookService.java
@@ -1,12 +1,52 @@
 package kr.co.book.admin.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.co.book.admin.dao.AdminBookDAO;
+import kr.co.book.admin.dto.AdminBookDTO;
+
 @Service
+@MapperScan(value = {"kr.co.book.admin.dao"}) 
 public class AdminBookService {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired AdminBookDAO adminBookDAO;
+
+	public HashMap<String, Object> adminBookList(String sPage, String searchText) {
+		HashMap<String, Object> adminBook = new HashMap<String, Object>();	
+		ArrayList<AdminBookDTO> list = null;
+		int page = Integer.parseInt(String.valueOf(sPage)); 
+		int offset = 10*(page-1);
+		int total = 0;
+		
+		if(searchText.equals("")) {
+			total = adminBookDAO.totalAdminBook();
+			list = adminBookDAO.adminBookList(offset);
+		}else {
+			HashMap<String, Object> data = new HashMap<String, Object>();
+			data.put("offset", offset);
+			data.put("searchText", searchText);
+			logger.info("data"+data);
+			total = adminBookDAO.totalAdminBookSearch(data);
+			list = adminBookDAO.adminBookListSearch(data);
+		}
+		
+		int range = total%9  == 0 ? total/9 : total/9+1;
+		page = page>range ? range:page;
+		adminBook.put("offset", offset);
+		adminBook.put("list", list);
+		adminBook.put("currPage", page);
+		adminBook.put("pages", range);
+		
+		return adminBook;
+	}
 
 }
