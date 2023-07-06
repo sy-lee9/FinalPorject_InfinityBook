5eddb5208aac4e1671b1155299d99ce255627cc8
leesuyeon <xmfhxmtm@gmail.com>
2023-07-06 11:49:30 +0900

    트래커 메모 삭제 기능 추가, 캘린더 페이지 추가

--- src/main/java/kr/co/book/mypage/controller/CalenderController.java
+++ src/main/java/kr/co/book/mypage/controller/CalenderController.java
@@ -3,10 +3,18 @@ package kr.co.book.mypage.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalenderController {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@RequestMapping(value = "/calender.go")
+	public String calenderPageGo() {
+		return "calender";
+	}
 
 }
--- src/main/webapp/views/index.jsp
+++ src/main/webapp/views/index.jsp
@@ -68,7 +68,7 @@
 								</li>
 								<li class="menu-item"><a href="#popular-books" class="nav-link" data-effect="Shop">공지사항</a></li>
 								<li class="menu-item"><a href="#latest-blog" class="nav-link" data-effect="Articles">이벤트</a></li>
-								<li class="menu-item"><a href="#contact" class="nav-link" data-effect="Contact">마이페이지</a></li>
+								<li class="menu-item"><a href="/libraryList.get" class="nav-link" data-effect="Contact">마이페이지</a></li>
 							</ul>
 
 							<div class="hamburger">
