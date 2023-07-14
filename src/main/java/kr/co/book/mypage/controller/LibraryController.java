c52d8c53d934d604337e0fcf16ffec316d1c7092
leemyunga <leemyunga0927@gmail.com>
2023-07-14 16:57:32 +0900

    위시 알림 등록

--- src/main/java/kr/co/book/mypage/controller/LibraryController.java
+++ src/main/java/kr/co/book/mypage/controller/LibraryController.java
@@ -128,7 +128,7 @@ public class LibraryController {
 			for (LibraryDTO libraryDTO : findWishList) {
 				logger.info("필요한 사람 : "+libraryDTO.getMember_idx());
 				
-				// libraryService.wishAarm(libraryDTO.getMember_idx(),String.valueOf(bookData.get("library_title")),String.valueOf(bookData.get("library_isbn")));
+				libraryService.wishAarm(libraryDTO.getMember_idx(),String.valueOf(bookData.get("library_title")),String.valueOf(bookData.get("library_isbn")));
 				
 			}
 		
