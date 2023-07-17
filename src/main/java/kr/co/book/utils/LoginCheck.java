f37f8bf334bdd363d153a36f0a5024753d1fc9df
leemyunga <leemyunga0927@gmail.com>
2023-07-17 13:46:18 +0900

    인터셉터 alert 추가

--- src/main/java/kr/co/book/utils/LoginCheck.java
+++ src/main/java/kr/co/book/utils/LoginCheck.java
@@ -12,18 +12,27 @@ public class LoginCheck implements HandlerInterceptor {
 
 	//preHandler : Controller 전에 이곳을 지나친다.
 	// true : 컨트롤러를 접근 허용 / false : 컨트롤러 접근 불가
+		
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		
-		boolean pass = true;
-		
-		HttpSession session = request.getSession();
-		if(session.getAttribute("loginIdx") == null) {
-			response.sendRedirect("/login.go");
-			pass = false;
-		}
-		
-		return pass;
-	}
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        boolean pass = true;
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loginIdx") == null) {
+            // alert 창을 포함한 JavaScript 코드를 추가한다.
+            String alertMessage = "로그인이 필요합니다.";
+            String redirectURL = "/login.go";
+            String script = "<script>alert('%s');location.href='%s';</script>";
+            String fullScript = String.format(script, alertMessage, redirectURL);
+
+            response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().println(fullScript);
+
+            pass = false;
+        }
+
+        return pass;
+    }
 }
